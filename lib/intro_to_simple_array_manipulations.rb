6a396f6a1c68366a4897b0f1cf69720300ce41f4
Nicholas Degrate <nicholasdegrate@gmail.com>
2021-01-26 22:40:53 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,15 @@
+
+
+def using_push(array, string )
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  return array.unshift(string)
+end
+
+def using_pop(array)
+  return array.pop
+end
+
+def pop_with
