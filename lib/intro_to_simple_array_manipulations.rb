5b0c6a8c0128ef52cdf7d8125cb929b4eba6ff85
Nicholas Degrate <nicholasdegrate@gmail.com>
2021-01-26 22:52:21 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,25 @@
+
+
+def using_push(array, string )
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  return array.unshift(string)
+end
+
+def using_pop(array)
+  return array.pop
+end
+
+def pop_with_args(array, num=2)
+  return array.pop(num)
+end
+
+def using_shift(array)
+  return array.shift()
+end
+
+def shift_with_args(array, num=2)
+  return array.shift(num)
+end
